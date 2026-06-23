2f5229786b42f2e0a4ced1b92f9bdd79383a7dda
tasumanian <nekohaimas@gmail.com>
2026-06-23 23:16:43 +0900

    アイテム.scの実装var1.0
    アイテム.scの実装var1.0

---
+++ Assets/Scripts/Taisei/Item.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObject/Item")]
+
+public class Item : ScriptableObject
+{
+    //名前
+    [SerializeField]
+    private string itemName;
+    public string ItemName
+    {
+        get { return itemName; }
+    }
+    [SerializeField]
+    private Sprite visual;
+    public Sprite Visual
+    {
+        get { return visual; }
+    }
+    //属性
+    [SerializeField]
+    private Attribute attribute;
+    public Attribute Attribute
+    {
+        get { return attribute; }
+    }
+}
+
+//アイテムの属性
+//追加する場合は名前とカンマを追加すること
+public enum Attribute
+{
+    hage,
+    hige,
+    huge,
+    hege,
+    hoge
+
+}
