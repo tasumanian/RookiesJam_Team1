ac374b2d042e9e6161a0b4b873239bb476332554
tasumanian <nekohaimas@gmail.com>
2026-06-28 12:23:40 +0900

    裁判フェーズの討論作成
    裁判フェーズの討論作成

---
+++ Assets/Scripts/Taisei/judge/Debate.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "CreateDebate", menuName = "ScriptableObject/Debate")]
+public class Debate : ScriptableObject
+{
+    [SerializeField]
+    private string statement;
+    public string Statement
+    {
+        get { return statement; }
+    }
+    [SerializeField]
+    private DebateAction actionType;
+    public DebateAction ActionType
+    {
+        get { return actionType; }
+    }
+    [SerializeField]
+    private string[] choiceText = new string[4];
+    public string[] ChoiceText
+    {
+        get { return choiceText; }
+    }
+    [SerializeField]
+    private int answerChoice;
+    public int AnswerChoice
+    {
+        get { return answerChoice; }
+    }
+    [SerializeField]
+    private Item answerItem;
+    public Item AnswerItem
+    {
+        get { return answerItem; }
+    }
+}
+public enum DebateAction
+{
+    choice,
+    selectitem
+
+}
