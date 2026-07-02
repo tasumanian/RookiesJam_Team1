7db554873837f09ef061aa1f87f8e6d09324304b
tasumanian <nekohaimas@gmail.com>
2026-07-02 14:24:01 +0900

    ゲームエンドボタンの実装
    ゲームエンドボタンの実装

--- Assets/Scripts/Yuichi/Scene/Main Game Move Script.cs
+++ Assets/Scripts/Yuichi/Scene/Main Game Move Script.cs
@@ -6,13 +6,13 @@ public class MainGameMoveScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void StartScene()
     {
@@ -23,5 +23,14 @@ public class MainGameMoveScript : MonoBehaviour
     {
 
         SceneManager.LoadScene("MainScene");
+    }
+    public void GameEnd()
+    {
+        //ゲームプレイ終了
+
+        Application.Quit();
+
+        //エディター上で止める用
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 }
